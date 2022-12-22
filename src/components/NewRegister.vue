bb6f8fc309341bdb8e22338b982b3b9e2c1bd8e9
Carlos Erroz <carlos.erroz@hotmail.com>
2022-12-22 23:37:35 +0100

    Jueves 22dic, signUp hecho

--- src/components/NewRegister.vue
+++ src/components/NewRegister.vue
@@ -2,7 +2,10 @@
     <form @submit.prevent="register" class="bg-sky-400 mx-auto rounded-md p-5 m-5 shadow-xl flex flex-col">
         <h3 class="mb-5 font-black text-sky-900 text-xl">Register</h3>
         <div v-if="errorMsg">
-            <p class="text-red-600">{{ errorMsg }}</p>
+            <p class="text-red-600 font-bold">{{ errorMsg }}</p>
+        </div>
+        <div v-if="okMsg">
+            <p class="text-green-700 font-bold">{{ okMsg }}</p>
         </div>
         <div>
             <label for="email">Email</label>
@@ -36,20 +39,17 @@ const email = ref(null);
 const password = ref(null);
 const repeatPassword = ref(null);
 const errorMsg = ref(null);
+const okMsg = ref(null);
 
-async function register() {
+function register() {
     if (password.value != repeatPassword.value) {
         errorMsg.value = "Error: passwords are not equal"
     } else if (password.value.length<6) {
         errorMsg.value = "Error: password length should be 6 at least"
     } else {
         errorMsg.value=null;
-        // try {
-            user.signUp(email,password);
-        //    if (error) throw error;
-        //} catch (error) {
-        //    errorMsg.value = error.message;
-        //}
+        okMsg.value="Sent, please validate it in your mailbox";
+        user.signUp(email.value,password.value);
     }
 
 }
